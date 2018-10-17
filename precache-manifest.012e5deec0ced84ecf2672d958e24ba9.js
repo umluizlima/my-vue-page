8a0786480e334d8693df99da3cf545b2c0ab5978
umluizlima <umluizlima@gmail.com>
2018-10-17 15:40:43 -0300

    620e72a

---
+++ precache-manifest.012e5deec0ced84ecf2672d958e24ba9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/my-vue-page/robots.txt"
+  },
+  {
+    "revision": "c862f565a4e19b7aec38",
+    "url": "/my-vue-page/js/chunk-vendors.59645627.js"
+  },
+  {
+    "revision": "98591ca3a0053670fd3d",
+    "url": "/my-vue-page/js/app.93e6886f.js"
+  },
+  {
+    "revision": "63252155dca5695b5b8c",
+    "url": "/my-vue-page/js/about.6fe3c8de.js"
+  },
+  {
+    "revision": "dc555156d70f4fc1238c2a76ddbf272d",
+    "url": "/my-vue-page/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/my-vue-page/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "98591ca3a0053670fd3d",
+    "url": "/my-vue-page/css/app.3b58b59a.css"
+  }
+];
